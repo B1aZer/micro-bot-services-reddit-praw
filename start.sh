04f4f86a06d47d0099f6180ef08089b84ce66ae1
concave-dash <kwerkee12@gmail.com>
2022-05-20 01:02:24 -0400

    log to log

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 source venv/bin/activate
-watchmedo3 auto-restart --pattern "*.py;*.js"--recurve --signal SIGTERM python3.9 handler.py
+watchmedo3 auto-restart --pattern "*.py;*.js"--recurve --signal SIGTERM python3.9 handler.py >> log.log
